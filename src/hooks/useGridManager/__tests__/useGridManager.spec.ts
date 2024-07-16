2f2b3d0fc5e7546c92f317389de171d4788177e0
Santiago Cuevas <santiagocuevas77@gmail.com>
2024-07-16 14:16:20 -0600

    implement basic navigation

--- src/hooks/useGridManager/__tests__/useGridManager.spec.ts
+++ src/hooks/useGridManager/__tests__/useGridManager.spec.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useGridManager } from "../useGridManager";
 
 describe("useGridManager", () => {
@@ -49,6 +49,130 @@ describe("useGridManager", () => {
       const { moveDown } = result.current;
 
       expect(typeof moveDown).toBe("function");
+    });
+  });
+
+  describe("navigation", () => {
+    describe("moveRight", () => {
+      it("increments the x index by 1", () => {
+        const { result } = renderHook(() => useGridManager());
+
+        act(() => {
+          result.current.moveRight();
+        });
+
+        expect(result.current.xIndex).toBe(1);
+      });
+
+      it("does not allow increments past a max value", () => {
+        const { result } = renderHook(() => useGridManager());
+        const maxIndex = 1;
+
+        act(() => {
+          result.current.moveRight(maxIndex);
+        });
+
+        act(() => {
+          result.current.moveRight(maxIndex);
+        });
+
+        act(() => {
+          result.current.moveRight(maxIndex);
+        });
+
+        expect(result.current.xIndex).toBe(maxIndex);
+      });
+    });
+
+    describe("moveLeft", () => {
+      it("decrements the x index by 1", () => {
+        const { result } = renderHook(() => useGridManager());
+
+        act(() => {
+          result.current.moveRight();
+        });
+
+        act(() => {
+          result.current.moveRight();
+        });
+
+        act(() => {
+          result.current.moveLeft();
+        });
+
+        expect(result.current.xIndex).toBe(1);
+      });
+
+      it("does not allow decrements into the negative", () => {
+        const { result } = renderHook(() => useGridManager());
+
+        act(() => {
+          result.current.moveLeft();
+        });
+
+        expect(result.current.xIndex).toBe(0);
+      });
+    });
+
+    describe("moveDown", () => {
+      it("increments the y index by 1", () => {
+        const { result } = renderHook(() => useGridManager());
+
+        act(() => {
+          result.current.moveDown();
+        });
+
+        expect(result.current.yIndex).toBe(1);
+      });
+
+      it("does not allow increments past a maxIndex", () => {
+        const { result } = renderHook(() => useGridManager());
+        const maxIndex = 1;
+
+        act(() => {
+          result.current.moveDown(maxIndex);
+        });
+
+        act(() => {
+          result.current.moveDown(maxIndex);
+        });
+
+        act(() => {
+          result.current.moveDown(maxIndex);
+        });
+
+        expect(result.current.yIndex).toBe(maxIndex);
+      });
+    });
+
+    describe("moveUp", () => {
+      it("decrements the y index by 1", () => {
+        const { result } = renderHook(() => useGridManager());
+
+        act(() => {
+          result.current.moveDown();
+        });
+
+        act(() => {
+          result.current.moveDown();
+        });
+
+        act(() => {
+          result.current.moveUp();
+        });
+
+        expect(result.current.yIndex).toBe(1);
+      });
+
+      it("does not allow decrements into the negative", () => {
+        const { result } = renderHook(() => useGridManager());
+
+        act(() => {
+          result.current.moveUp();
+        });
+
+        expect(result.current.yIndex).toBe(0);
+      });
     });
   });
 });
--- src/hooks/useGridManager/useGridManager.ts
+++ src/hooks/useGridManager/useGridManager.ts
@@ -4,13 +4,33 @@ export const useGridManager = () => {
   const [xIndex, setXIndex] = useState(0);
   const [yIndex, setYIndex] = useState(0);
 
-  const moveRight = () => {};
+  const moveRight = (maxIndex: number = Infinity) => {
+    const nextXIndex = xIndex + 1;
+    if (nextXIndex <= maxIndex) {
+      setXIndex(nextXIndex);
+    }
+  };
 
-  const moveLeft = () => {};
+  const moveLeft = () => {
+    const nextXIndex = xIndex - 1;
+    if (nextXIndex >= 0) {
+      setXIndex(nextXIndex);
+    }
+  };
 
-  const moveUp = () => {};
+  const moveUp = () => {
+    const nextYIndex = yIndex - 1;
+    if (nextYIndex >= 0) {
+      setYIndex(nextYIndex);
+    }
+  };
 
-  const moveDown = () => {};
+  const moveDown = (maxIndex: number = Infinity) => {
+    const nextYIndex = yIndex + 1;
+    if (nextYIndex <= maxIndex) {
+      setYIndex(yIndex + 1);
+    }
+  };
 
   return { xIndex, yIndex, moveRight, moveLeft, moveUp, moveDown };
 };
